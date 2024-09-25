82b6f9b740f3836a8b3ee0e5f4e32ae56b49a5aa
Sarah John <sarahjohn678@gmail.com>
2024-09-25 17:22:31 -0400

    activity 1.3 complete

--- hello.py
+++ hello.py
@@ -1,20 +1,27 @@
+from datetime import datetime
 from flask import Flask, render_template #Flask will look for templates subdir
 from flask_bootstrap import Bootstrap
+from flask_moment import Moment
+
 
 #application instance is an object of class Flask
 app = Flask(__name__) #takes the name of the main module as the required argument, using Python's name var here
 bootstrap = Bootstrap(app)
-@app.route('/')
-def index(): #index is the handler for the main root URL, view function
- return render_template('index.html')
-
-@app.route('/user/<name>') #dynamic, second route, respond with the personalized greeting using the name dynamic argument
-def user(name):
- return render_template('user.html', name=name)
+moment = Moment(app)
 
 @app.errorhandler(404)
 def page_not_found(e):
  return render_template('404.html'), 404
+
 @app.errorhandler(500)
 def internal_server_error(e):
  return render_template('500.html'), 500
+
+@app.route('/') #index is the handler for the main root URL, view function
+def index():
+ return render_template('index.html',
+ current_time=datetime.utcnow())
+
+@app.route('/user/<name>') #dynamic, second route, respond with the personalized greeting using the name dynamic argument
+def user(name):
+ return render_template('user.html', name=name)
