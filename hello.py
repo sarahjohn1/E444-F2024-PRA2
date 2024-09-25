c7c0db3aadc5a10bdaaac31c5a3234fb39fbb6d8
Sarah John <sarahjohn678@gmail.com>
2024-09-24 23:24:31 -0400

    example 3-5

--- hello.py
+++ hello.py
@@ -1,7 +1,9 @@
 from flask import Flask, render_template #Flask will look for templates subdir
+from flask_bootstrap import Bootstrap
 
 #application instance is an object of class Flask
 app = Flask(__name__) #takes the name of the main module as the required argument, using Python's name var here
+bootstrap = Bootstrap(app)
 @app.route('/')
 def index(): #index is the handler for the main root URL, view function
  return render_template('index.html')
