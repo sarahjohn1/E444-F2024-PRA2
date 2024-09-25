7bca86104f7307ee7da413668476941c871d5fe7
Sarah John <sarahjohn678@gmail.com>
2024-09-24 22:04:26 -0400

    comments

--- hello.py
+++ hello.py
@@ -1,5 +1,7 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask 
+
+#application instance is an object of class Flask
+app = Flask(__name__) #takes the name of the main module as the required argument, using Python's name var here
 @app.route('/')
-def index():
+def index(): #index is the handler for the main root URL, view function
  return '<h1>Hello World!</h1>'
