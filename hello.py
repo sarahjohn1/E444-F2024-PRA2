8cfa247b8a6b42c131bb0ebd5843c975a353d9c9
Sarah John <sarahjohn678@gmail.com>
2024-09-25 00:10:53 -0400

    adding base and custom 404

--- hello.py
+++ hello.py
@@ -11,3 +11,10 @@ def index(): #index is the handler for the main root URL, view function
 @app.route('/user/<name>') #dynamic, second route, respond with the personalized greeting using the name dynamic argument
 def user(name):
  return render_template('user.html', name=name)
+
+@app.errorhandler(404)
+def page_not_found(e):
+ return render_template('404.html'), 404
+@app.errorhandler(500)
+def internal_server_error(e):
+ return render_template('500.html'), 500
