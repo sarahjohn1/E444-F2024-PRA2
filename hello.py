5f6f40ab458cc63b5c5d680c07ebc688cdbb354b
Sarah John <sarahjohn678@gmail.com>
2024-09-24 22:54:35 -0400

    example 3-2

--- hello.py
+++ hello.py
@@ -1,11 +1,11 @@
-from flask import Flask 
+from flask import Flask, render_template #Flask will look for templates subdir
 
 #application instance is an object of class Flask
 app = Flask(__name__) #takes the name of the main module as the required argument, using Python's name var here
 @app.route('/')
 def index(): #index is the handler for the main root URL, view function
- return '<h1>Hello World!</h1>'
+ return render_template('index.html')
 
-@app.route('/user/<name>') #dynamic, second route
+@app.route('/user/<name>') #dynamic, second route, respond with the personalized greeting using the name dynamic argument
 def user(name):
- return '<h1>Hello, {}!</h1>'.format(name)
+ return render_template('user.html', name=name)
