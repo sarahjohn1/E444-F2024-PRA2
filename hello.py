94421ef57cf6502defdf685e560f339735e52b3a
Sarah John <sarahjohn678@gmail.com>
2024-09-25 21:21:58 -0400

    example 4.7 reproduced

--- hello.py
+++ hello.py
@@ -1,5 +1,6 @@
 from datetime import datetime
-from flask import Flask, render_template #Flask will look for templates subdir
+# Flask will look for templates subdir
+from flask import Flask, render_template, session, redirect, url_for, flash
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
 from flask_wtf import FlaskForm
@@ -7,29 +8,43 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 
 
-#application instance is an object of class Flask
-app = Flask(__name__) #takes the name of the main module as the required argument, using Python's name var here
+# application instance is an object of class Flask
+# takes the name of the main module as the required argument, using Python's name var here
+app = Flask(__name__)
 app.config['SECRET_KEY'] = 'hard to guess string'
 bootstrap = Bootstrap(app)
 moment = Moment(app)
 
+
 @app.errorhandler(404)
 def page_not_found(e):
- return render_template('404.html'), 404
+    return render_template('404.html'), 404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
- return render_template('500.html'), 500
+    return render_template('500.html'), 500
 
-@app.route('/') #index is the handler for the main root URL, view function
-def index():
- return render_template('index.html',
- current_time=datetime.utcnow())
 
-@app.route('/user/<name>') #dynamic, second route, respond with the personalized greeting using the name dynamic argument
+# dynamic, second route, respond with the personalized greeting using the name dynamic argument
+@app.route('/user/<name>')
 def user(name):
- return render_template('user.html', name=name)
+    return render_template('user.html', name=name)
 
- class NameForm(FlaskForm):
+
+class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    form = NameForm()
+    if form.validate_on_submit():
+        old_name = session.get('name')
+        if old_name is not None and old_name != form.name.data:
+            flash('Looks like you have changed your name!')
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html',
+                           form=form, name=session.get('name'))
